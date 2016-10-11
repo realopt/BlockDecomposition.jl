98caa15b94b099fdc65dcb8464ae5c90099dbc69
Guillaume Marques <guillaume.marques@math.u-bordeaux.fr>
2016-10-11 14:55:59 +0200

    Improve cur cost update

--- src/bjmodel.jl
+++ src/bjmodel.jl
@@ -15,45 +15,20 @@ function BlockModel(;solver = JuMP.UnsetSolver(),
   m.ext[:BlockIdentification] = BlockIdentificationData(nb_block_indices,
                                                         block_group_func,
                                                         block_group_lb_func,
-                                                        block_group_ub_func    )
+                                                        block_group_ub_func   )
   # Adding variable names and constraint names
   m.ext[:VarNames] = m.varDict
   m.ext[:ConstrNames] = m.conDict
-  m.ext[:CurCost] = Vector{Float64}()
+  m.ext[:CurCost] = Array(Cdouble, m.numCols)
   m
 end
 
-_similar(x::Array) = Array(Float64,size(x))
-_similar{T}(x::Dict{T}) = Dict{T,Float64}()
-_getCurCost(x::JuMP.Variable) = x.m.ext[:CurCost][x.col]
-
-function _getCurCostInner(x)
-  vars = x.innerArray
-  costs = _similar(vars)
-  for i in eachindex(vars)
-    costs[i] = _getCurCost(vars[i])
-  end
-  costs
-end
-
 function getcurcost(x::JuMP.Variable)
-  cc = _getCurCost(x)
-  # add warning if is NaN
+  cc = x.m.ext[:CurCost][x.col]
+  (cc == NaN) && warn("current cost for the variable $x is not defined.")
+  cc
 end
-
-JuMPContainer_from(x::JuMP.JuMPDict,inner) = JuMP.JuMPDict(inner)
-JuMPContainer_from(x::JuMP.JuMPArray,inner) = JuMP.JuMPArray(inner, x.indexsets)
 
 function getcurcost(arr::Array{JuMP.Variable})
   error("getcurcost(Array{JuMP.Variable}) is not implemented")
-end
-
-function getcurcost(x::JuMP.JuMPContainer{JuMP.Variable})
-  ret = JuMPContainer_from(x, _getCurCostInner(x))
-  for (key, val) in x.meta
-    ret.meta[key] = val
-  end
-  m = x.meta[:model] #getmeta
-  m.varData[ret] = x.meta[:model].varData[x] #printdata
-  ret
 end
