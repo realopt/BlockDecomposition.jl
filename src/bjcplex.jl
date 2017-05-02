f4f32da9a799ef6958971e567d404b7da660c747
Guillaume Marques <guillaume.marques@math.u-bordeaux.fr>
2017-05-02 14:02:59 +0200

    Apply Cplex B dec only if the solver is cplex

--- src/bjcplex.jl
+++ src/bjcplex.jl
@@ -2,25 +2,27 @@ defineannotations() = nothing
 
 @require CPLEX begin
   function defineannotations(m::JuMP.Model, vars_decomposition)
-    JuMP.build(m)
-    model = m.internalModel
-    blocks = Dict{Tuple,Clong}()
-    iblock = 1
-    newlongannotation(model.inner, "cpxBendersPartition", Clong(-1))
-    for (col, (v_name, v_id, sp_type, sp_id)) in enumerate(vars_decomposition)
-        indexArr = Array(Cint,1)
-        indexArr[1] = col - 1
-        valArr = Array(Clong,1)
-        if sp_type == :B_MASTER
-            valArr[1] = 0
-        elseif sp_type == :B_SP
-            if !haskey(blocks, sp_id)
-                blocks[sp_id] = iblock
-                iblock += 1
-            end
-            valArr[1] = blocks[sp_id]
-        end
-        setlongannotations(model.inner, Cint(0), Cint(1), Cint(1), indexArr, valArr)
+    if isa(m.solver, CPLEX.CplexSolver)
+      JuMP.build(m)
+      model = m.internalModel
+      blocks = Dict{Tuple,Clong}()
+      iblock = 1
+      newlongannotation(model.inner, "cpxBendersPartition", Clong(-1))
+      for (col, (v_name, v_id, sp_type, sp_id)) in enumerate(vars_decomposition)
+          indexArr = Array(Cint,1)
+          indexArr[1] = col - 1
+          valArr = Array(Clong,1)
+          if sp_type == :B_MASTER
+              valArr[1] = 0
+          elseif sp_type == :B_SP
+              if !haskey(blocks, sp_id)
+                  blocks[sp_id] = iblock
+                  iblock += 1
+              end
+              valArr[1] = blocks[sp_id]
+          end
+          setlongannotations(model.inner, Cint(0), Cint(1), Cint(1), indexArr, valArr)
+      end
     end
   end
 end
